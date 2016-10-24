d2f8808c7c0d8a96d20a9f0393c715cdb13febfd
“jaisonfdo” <jaison@nfnlabs.in>
2016-10-24 22:36:12 +0530

    Initial Commit

---
+++ app/src/main/java/droidmentor/bnv_with_viewpager/MainActivity.java
@@ -0,0 +1,113 @@
+package droidmentor.bnv_with_viewpager;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
+
+import droidmentor.bnv_with_viewpager.Fragment.CallsFragment;
+import droidmentor.bnv_with_viewpager.Fragment.ChatFragment;
+import droidmentor.bnv_with_viewpager.Fragment.ContactsFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    //This is our viewPager
+    private ViewPager viewPager;
+
+
+    //Fragments
+
+    ChatFragment chatFragment;
+    CallsFragment callsFragment;
+    ContactsFragment contactsFragment;
+    MenuItem prevMenuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        //Initializing the bottomNavigationView
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_call:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.action_chat:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.action_contact:
+                                viewPager.setCurrentItem(2);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                Log.d("page", "onPageSelected: "+position);
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+       /*  //Disable ViewPager Swipe
+
+       viewPager.setOnTouchListener(new View.OnTouchListener()
+        {
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                return true;
+            }
+        });
+
+        */
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        callsFragment=new CallsFragment();
+        chatFragment=new ChatFragment();
+        contactsFragment=new ContactsFragment();
+        adapter.addFragment(callsFragment);
+        adapter.addFragment(chatFragment);
+        adapter.addFragment(contactsFragment);
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/droidmentor/bnv_with_viewpager/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package droidmentor.bnv_with_viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Jaison on 23/10/16.
+ */
+
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFragment(Fragment fragment) {
+        mFragmentList.add(fragment);
+    }
+
+}
